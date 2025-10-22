dfb41fc84e94ddca14a193adcf165910a707c212
eddie1031 <archaco123@gmail.com>
2025-10-22 23:15:51 +0900

    Init

---
+++ src/main/java/io/eddie/pgexp/PgExpApplication.java
@@ -0,0 +1,13 @@
+package io.eddie.pgexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgExpApplication.class, args);
+    }
+
+}
