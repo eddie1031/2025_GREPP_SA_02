93f0389066dedbb49fa6c8f203e20c6ee838dd4e
eddie1031 <archaco123@gmail.com>
2025-10-23 13:37:57 +0900

    ~complete

--- src/main/java/io/eddie/pgexp/PaymentController.java
+++ src/main/java/io/eddie/pgexp/PaymentController.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,20 +18,51 @@ import org.springframework.web.client.RestTemplate;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class PaymentController {
 
+    @Value("${payment.secretKey}")
+    private String secretKey;
+
+    @Value("${payment.targetUrl}")
+    private String targetUrl;
+
     private final RestTemplate restTemplate;
     private final ObjectMapper om = new ObjectMapper();
 
-    @GetMapping("/success")
-    public String success() {
-        return "success";
+    private static final String PRODUCT_NAME = "프리미엄 무선 이어폰";
+    private static final String ORDER_ID = UUID.randomUUID().toString();
+    private static final Long AMOUNT = 100L;
+
+    @GetMapping("/")
+    public String showIndex(Model model) {
+        model.addAttribute("orderId", ORDER_ID);
+        model.addAttribute("productName", PRODUCT_NAME);
+        model.addAttribute("amount", AMOUNT);
+        return "index";
     }
 
+    @GetMapping("/fail")
+    public String showFail(Model model) {
+        model.addAttribute("orderId", ORDER_ID);
+        model.addAttribute("productName", PRODUCT_NAME);
+        model.addAttribute("amount", AMOUNT);
+        return "fail";
+    }
+
+    @GetMapping("/success")
+    public String showSuccess(Model model) {
+
+        model.addAttribute("orderId", ORDER_ID);
+        model.addAttribute("productName", PRODUCT_NAME);
+        model.addAttribute("amount", AMOUNT);
+
+        return "success";
+    }
 
     public static record TossPaymentRequest(
             String paymentKey,
@@ -43,11 +76,8 @@ public class PaymentController {
             @RequestBody TossPaymentRequest request
     ) {
 
-        // 시크릿키 작업
-        String widgetSecretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
-
 //        String target = "Basic %s:".formatted(widgetSecretKey);
-        String target = widgetSecretKey + ":";
+        String target = secretKey + ":";
 
         Base64.Encoder encoder = Base64.getEncoder();
         String encryptedSecretKey = "Basic " + encoder.encodeToString(target.getBytes(StandardCharsets.UTF_8));
@@ -63,7 +93,7 @@ public class PaymentController {
         HttpEntity<Map<String, Object>> httpEntity = new HttpEntity<>(requestMap, httpHeaders);
 
         ResponseEntity<String> response = restTemplate.exchange(
-                "https://api.tosspayments.com/v1/payments/confirm",
+                targetUrl,
                 HttpMethod.POST,
                 httpEntity,
                 String.class
